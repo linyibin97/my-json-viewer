dc91fc55adccf7d31e8b5805b9aa866b6a3f395d
yibinllin <yibinllin@tencent.com>
2023-11-05 21:06:25 +0800

    chore: build

---
+++ src/components/Navigator/index.tsx
@@ -0,0 +1,15 @@
+// import { useDispatch } from "react-redux";
+// import { toggleDarkMode } from "../../store/reducer/theme";
+
+const Navigator = () => {
+  // const dispatch = useDispatch();
+  // const handleToggleDarkMode = () => {
+  //   dispatch(toggleDarkMode());
+  // };
+
+  return (
+    <div>hello</div>
+  );
+};
+
+export default Navigator;
