4968d64576cb5d60dc05c42bc5f69aaa050fd64f
yibinllin <yibinllin@tencent.com>
2023-11-05 22:45:40 +0800

    fix: css import

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import "./index.css";
+import "normalize.css/normalize.css"
+import "@blueprintjs/core/lib/css/blueprint.css"
+import "@blueprintjs/icons/lib/css/blueprint-icons.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-json-viewer/',
   plugins: [react()],
   css: {
     postcss: {
