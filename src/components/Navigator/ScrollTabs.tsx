a5bbd7b3db361fd3e4a4c2dbf3fecf0008ae333c
yibinllin <linyibin1997@126.com>
2023-11-07 00:25:23 +0800

    feat: scroll bar

--- src/components/Navigator/ScrollTabs.tsx
+++ src/components/Navigator/ScrollTabs.tsx
@@ -1,19 +1,48 @@
+import { useState } from "react";
+
+const Tab = (props: { title: string }) => {
+  const { title } = props;
+  return (
+    <div className="mx-1 px-2 py-2 truncate shrink-0 max-w-[8rem] cursor-pointer h-full select-none">
+      {title}
+    </div>
+  );
+};
+
+let scrollbarTimer: number | null = null;
+
 const ScrollTabs = () => {
+  const [isShowScrollBar, setIsShowScrollbar] = useState<boolean>(false);
+
+  const showScrollBar = () => {
+    if (scrollbarTimer) {
+      clearTimeout(scrollbarTimer);
+    } else {
+      setIsShowScrollbar(true);
+    }
+    scrollbarTimer = setTimeout(() => {
+      setIsShowScrollbar(false);
+      scrollbarTimer = null;
+    }, 500);
+  };
+
   const handleWheel = (event: React.WheelEvent<HTMLDivElement>) => {
-    console.log(event);
+    showScrollBar()
     const container = event.currentTarget;
     const delta = Math.sign(event.nativeEvent.deltaY);
-    container.scrollLeft += delta * 100; // 控制滚动速度
-    event.preventDefault(); // 阻止页面滚动
+    container.scrollLeft += delta * 100;
   };
 
   return (
     <div
-      className="h-full flex flex-row items-center overflow-x-auto hide-scrollbar"
+      className={
+        "h-full flex flex-row items-center overflow-x-auto mini-scrollbar" +
+        (isShowScrollBar ? " mini-scrollbar-show" : "")
+      }
       onWheel={handleWheel}
     >
       {new Array(20).fill(null).map((_, index) => (
-        <div key={index} className="mx-2">{`${index}.JSON`}</div>
+        <Tab title={`Untitled-${index}.json`} key={index}/>
       ))}
     </div>
   );
--- src/components/Navigator/index.tsx
+++ src/components/Navigator/index.tsx
@@ -1,19 +1,18 @@
 import LeftContent from "./LeftContent";
 import RightContent from "./RightContent";
 import ScrollTabs from "./ScrollTabs";
-import './index.css'
+import "./index.css";
 
 const Navigator = () => {
-
   return (
-    <div className="bp5-navbar flex items-center">
-      <div className="h-full w-24 border">
+    <div className="bp5-navbar flex items-center" style={{ padding: 0, height: '2.5rem' }}>
+      <div className="h-full w-24 bg-slate-400 opacity-50">
         <LeftContent />
       </div>
       <div className="h-full flex-1 overflow-hidden">
         <ScrollTabs />
       </div>
-      <div className="h-full w-36 border">
+      <div className="h-full w-24 bg-slate-400 opacity-50">
         <RightContent />
       </div>
     </div>
